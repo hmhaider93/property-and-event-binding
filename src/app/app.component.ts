9e4be142800488a6d6deca22d69cddc2e2da8847
Haider Mushtaq <hmhaider93@gmail.com>
2022-05-21 08:31:37 +0200

    Basic setup complete also application split

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  serverElements = [{type:'server',name:'TestServer1',content:'This server is online!'}];
+
+
+
+}
