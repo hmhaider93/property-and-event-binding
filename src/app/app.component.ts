5d79e88ad015781a44ae9e36bd412b5e97c05ef5
Haider Mushtaq <hmhaider93@gmail.com>
2022-05-23 17:33:29 +0200

    created custom directives for menu dropdowns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,35 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  serverElements = [{type:'server',name:'TestServer1',content:'This server is online!'}];
+  gameData = [{ type: 'odd', num: 3 }];
 
-  gameData = [{type:'odd',num:3}];
-
-  onServerAdded(serverData:{serverName:string,serverContent:string}){
-      this.serverElements.push({
-      type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent
-    });
+  onCheckNumber(input: number) {
+    if (input % 2 === 0) {
+      this.gameData.push({ type: 'even', num: input });
+    } else this.gameData.push({ type: 'odd', num: input });
   }
-
-  onBlueprintAdded(serverData:{serverName:string,serverContent:string}){
-    this.serverElements.push({
-      type: 'blueprint',
-      name: serverData.serverName,
-      content: serverData.serverContent
-    });
-  }
-
-  onCheckNumber(input:number){
-    if(input%2 === 0){
-      this.gameData.push({type:'even', num:input})
-    }else this.gameData.push({type:'odd', num:input})
-  }
-
-
-
 }
